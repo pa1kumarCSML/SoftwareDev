100e5a4144bb85f10da26eaa46c9106b3258e3f8
pavan kumar <dpavankumar8797@gmail.com>
2023-03-22 14:34:10 +0530

    Update server.js

--- expMongoDb/backend/server.js
+++ expMongoDb/backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+const express = require("express");
+const dotenv = require("dotenv");
+const port = process.env.PORT || 5000;
+
+
+const app = new express();
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
