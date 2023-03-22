c5d37aa4b061b03388bc8feb54264665fa7231da
pavan kumar <dpavankumar8797@gmail.com>
2023-03-22 14:09:33 +0530

    setup

--- expressJS/index.js
+++ expressJS/index.js
@@ -7,20 +7,24 @@ const app = new express();
 
 const PORT = process.env.PORT || 5000;
 
+
+//Body Parse Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //default home
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 //**Set static folder
-
 app.use(express.static(path.join(__dirname, "public")));
 
 // init middleware
 //app.use(logger);//executes whenever there is a request.
 
 //REST API
-
 app.use("/api/members", require("./routes/api/members"));
 
+//Listen's for requests
 app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) });
--- expressJS/routes/api/members.js
+++ expressJS/routes/api/members.js
@@ -1,6 +1,8 @@
 const Express = require("express");
-const members = require("../../Members");
+const uuid = require("uuid");
 
+const members = require("../../Members");
+//router api
 const router = new Express.Router();
 
 //Get all members
@@ -17,6 +19,60 @@ router.get("/:id", (req, res) => {
     } else {
         res.status(400).json({ message: `No member with the id ${req.params.id}` });
     }
+});
+
+//create member(post method)
+
+router.post("/", (req, res) => {
+    //console.log(req.body);
+    const newMem = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: "Active"
+    };
+    if (!newMem.name || !newMem.email) {
+        res.status(400).json({ message: "Please include name and email id" });
+    } else {
+        members.push(newMem);
+    }
+    res.send(members);
+});
+
+//create member(put method)
+
+router.put("/:id", (req, res) => {
+    //console.log(req.body);
+    const updMem = req.body;
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found) {
+        members.forEach(member => {
+            if (member.id === parseInt(req.params.id)) {
+                member.name = updMem.name ? updMem.name : member.name;
+                member.email = updMem.email ? updMem.email : member.email;
+                res.json({ message: "member updated", member });
+            }
+        });
+    }
+    else {
+        res.status(400).json({ message: "Please include name and email id" });
+    }
+    res.send(members);
+});
+
+router.delete("/:id", (req, res) => {
+    //console.log(req.body);
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found) {
+        res.json({
+            message: "member deleted",
+            member: members.filter(member => member.id !== parseInt(req.params.id))
+        });
+    }
+    else {
+        res.status(400).json({ message: "Please include name and email id" });
+    }
+    res.send(members);
 });
 
 module.exports = router;
